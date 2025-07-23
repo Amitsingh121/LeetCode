abbf9e5790507da09f9322acd23a3730f1010cbb
Amitsingh121 <amitksingh7779@gmail.com>
2025-07-24 00:12:20 +0530

    enhance arry concept

--- LeetCode/src/src/array/FirstNumber.java
+++ LeetCode/src/src/array/FirstNumber.java
@@ -1,4 +1,24 @@
 package src.array;
 
 public class FirstNumber {
+    public static void main(String[] args){
+        int[] arr ={43,22,13,44};
+        for(int i = 0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("Enhanced For Loop");
+
+        for(int a : arr){
+            a = a+1;
+            System.out.println(a);
+        }
+
+        System.out.println("new array resut");
+
+        for(int a : arr){
+
+            System.out.println(a);
+        }
+    }
 }
