a3e62a002d0b2f89f1b66c1f41d76cb230bd655c
Amitsingh121 <amitksingh7779@gmail.com>
2025-07-24 22:37:34 +0530

    second method

--- LeetCode/src/src/LeetCode/stockSell.java
+++ LeetCode/src/src/LeetCode/stockSell.java
@@ -1,22 +1,39 @@
+
 package src.LeetCode;
 
 public class stockSell {
 
     public static int maxProfit(int[] arr) {
 
-        int n= arr.length;
-        int minSoFar=arr[0];
-        int maxProfit=0;
-        for(int i=0;i<arr.length;i++){
-            minSoFar=Math.min(minSoFar, arr[i]);
-            maxProfit = Math.max((arr[i]-minSoFar),maxProfit);
+        /*
+        logic: Track the minimum price so far and calculate max profit at each step.
+        int n = arr.length;
+        int minSoFar = arr[0];
+        int maxProfit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            minSoFar = Math.min(minSoFar, arr[i]);
+            maxProfit = Math.max((arr[i] - minSoFar), maxProfit);
+        }
+        return maxProfit;
+        */
+
+        // Alternative method: Use two pointers (buy and sell) to find the best profit window.
+        int buy = 0, sell = 1, maxProfit = 0;
+        while (sell < arr.length) {
+            if (arr[sell] > arr[buy]) {
+                maxProfit = Math.max(maxProfit, arr[sell] - arr[buy]);
+            } else {
+                buy = sell;
+            }
+            sell++;
         }
         return maxProfit;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,33,4,66,8};
+        int[] arr = {1, 33, 4, 66, 8};
         int profit = maxProfit(arr);
         System.out.println(profit);
     }
 }
+
